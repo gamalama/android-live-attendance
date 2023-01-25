b2b0d5b263eb08462be75c9312ba606ea5095673
Fakhry <fakhry.linux@gmail.com>
2023-01-25 22:56:52 +0800

    ui: add login page

---
+++ app/src/main/java/me/fakhry/androidliveattendance/views/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package me.fakhry.androidliveattendance.views.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.fakhry.androidliveattendance.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
