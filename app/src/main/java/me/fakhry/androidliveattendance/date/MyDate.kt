f298658825d7a59a9827f1b287724fd266613f8f
Fakhry <fakhry.linux@gmail.com>
2023-01-30 15:29:18 +0800

    feat: Feature Attendance

---
+++ app/src/main/java/me/fakhry/androidliveattendance/date/MyDate.kt
@@ -0,0 +1,12 @@
+package me.fakhry.androidliveattendance.date
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object MyDate {
+    fun getCurrentDateForServer(): String {
+        val currentTime = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        return dateFormat.format(currentTime)
+    }
+}
